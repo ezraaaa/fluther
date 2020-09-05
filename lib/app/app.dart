7065a0572511e5470c2aa8e48ee869201a3c70c2
Ezra Gunn <ezracodes@gmail.com>
2020-09-05 17:13:59 +0800

    Added annotatedregion, title and theme

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,8 +1,24 @@
+import 'package:fluther/main/flavour_config.dart';
+import 'package:fluther/resources/strings.dart';
+import 'package:fluther/resources/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: const SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        systemNavigationBarColor: Colors.transparent,
+        systemNavigationBarDividerColor: Colors.transparent,
+      ),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: FlavourConfig.isDevelopment(),
+        title:
+            '${strings.appTitle} ${FlavourConfig.instance.values.currentFlavour}',
+        theme: getThemeData(context),
+      ),
+    );
   }
 }
