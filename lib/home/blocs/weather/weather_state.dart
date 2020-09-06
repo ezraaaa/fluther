a75662126056c2a0ffb9eb54968bd49536e17dff
Ezra Gunn <ezracodes@gmail.com>
2020-09-06 16:36:26 +0800

    Added states

--- lib/home/blocs/weather/weather_state.dart
+++ lib/home/blocs/weather/weather_state.dart
@@ -2,9 +2,21 @@ part of 'weather_bloc.dart';
 
 abstract class WeatherState extends Equatable {
   const WeatherState();
-  
+
   @override
   List<Object> get props => [];
 }
 
 class WeatherInitial extends WeatherState {}
+
+class WeatherLoadInProgress extends WeatherState {}
+
+class WeatherLoadSuccess extends WeatherState {
+  const WeatherLoadSuccess({@required this.weather}) : assert(weather != null);
+  final Weather weather;
+
+  @override
+  List<Object> get props => <Weather>[weather];
+}
+
+class WeatherLoadFailure extends WeatherState {}
