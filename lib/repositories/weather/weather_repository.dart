1ac222a0b46b7ea243add93a8a8da4b415e3689f
Ezra Gunn <ezracodes@gmail.com>
2020-09-07 00:26:55 +0800

    Change to one call api

--- lib/repositories/weather/weather_repository.dart
+++ lib/repositories/weather/weather_repository.dart
@@ -2,18 +2,21 @@ import 'dart:convert' as convert;
 
 import 'package:fluther/home/models/location_weather/location_weather.dart';
 import 'package:http/http.dart' as http;
+import 'package:meta/meta.dart';
 
 class WeatherRepository {
   static const String baseUrl = 'https://api.openweathermap.org/data/2.5';
   static const String apiKey = 'a54311863e7e38a6674b331d7a1b871e';
 
-  Future<LocationWeather> fetchCityWeather(String cityName) async {
-    final String locationUrl = '$baseUrl/weather?q=$cityName&appid=$apiKey';
+  Future<LocationWeather> fetchCityWeather(
+      {@required double latitude, @required double longitude}) async {
+    final String locationUrl =
+        '$baseUrl/onecall?lat=$latitude&lon=$longitude&units=metric&appid=$apiKey';
     final http.Response weatherResponse = await http.get(locationUrl);
 
     if (weatherResponse.statusCode == 200) {
       final Map<String, dynamic> weatherJson =
-          convert.jsonDecode(weatherResponse.body);
+          convert.jsonDecode(weatherResponse.body) as Map<String, dynamic>;
       return LocationWeather.fromJson(weatherJson);
     } else {
       final int statusCode = weatherResponse.statusCode;
