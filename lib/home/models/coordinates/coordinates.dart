385eba4b10659aa6a172cf817bfdc6471ca60de6
Ezra Gunn <ezracodes@gmail.com>
2020-09-06 16:36:04 +0800

    Added generated file

--- lib/home/models/coordinates/coordinates.dart
+++ lib/home/models/coordinates/coordinates.dart
@@ -1,6 +1,14 @@
+import 'package:json_annotation/json_annotation.dart';
+part 'coordinates.g.dart';
+
+@JsonSerializable()
 class Coordinates {
+  Coordinates({this.lon, this.lat});
+
+  factory Coordinates.fromJson(Map<String, dynamic> json) =>
+      _$CoordinatesFromJson(json);
+  Map<String, dynamic> toJson() => _$CoordinatesToJson(this);
+
   double lon;
   double lat;
-
-  Coordinates({this.lon, this.lat});
 }
---
+++ lib/home/models/coordinates/coordinates.g.dart
@@ -0,0 +1,20 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'coordinates.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Coordinates _$CoordinatesFromJson(Map<String, dynamic> json) {
+  return Coordinates(
+    lon: (json['lon'] as num)?.toDouble(),
+    lat: (json['lat'] as num)?.toDouble(),
+  );
+}
+
+Map<String, dynamic> _$CoordinatesToJson(Coordinates instance) =>
+    <String, dynamic>{
+      'lon': instance.lon,
+      'lat': instance.lat,
+    };
