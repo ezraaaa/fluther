0492cb4de051cacc73688b3bda41ab26f32fcf97
Ezra Gunn <ezracodes@gmail.com>
2020-09-06 16:36:40 +0800

    Added events

--- lib/home/blocs/weather/weather_event.dart
+++ lib/home/blocs/weather/weather_event.dart
@@ -2,7 +2,20 @@ part of 'weather_bloc.dart';
 
 abstract class WeatherEvent extends Equatable {
   const WeatherEvent();
+}
+
+class WeatherRequested extends WeatherEvent {
+  const WeatherRequested({@required this.city}) : assert(city != null);
+  final String city;
 
   @override
-  List<Object> get props => [];
+  List<Object> get props => <String>[city];
+}
+
+class WeatherRefreshRequested extends WeatherEvent {
+  const WeatherRefreshRequested({@required this.city}) : assert(city != null);
+  final String city;
+
+  @override
+  List<Object> get props => <String>[city];
 }
