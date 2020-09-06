f3cb386578501070cacf8019fa48d0713a64b9c6
Ezra Gunn <ezracodes@gmail.com>
2020-09-06 17:53:30 +0800

    Added fetch weather logic

--- lib/home/blocs/weather/weather_bloc.dart
+++ lib/home/blocs/weather/weather_bloc.dart
@@ -2,17 +2,52 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:fluther/home/models/location_weather/location_weather.dart';
+import 'package:fluther/repositories/weather/weather_repository.dart';
+import 'package:meta/meta.dart';
 
 part 'weather_event.dart';
 part 'weather_state.dart';
 
 class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
-  WeatherBloc() : super(WeatherInitial());
+  WeatherBloc({@required this.weatherRepository})
+      : assert(weatherRepository != null),
+        super(WeatherInitial());
+
+  final WeatherRepository weatherRepository;
 
   @override
   Stream<WeatherState> mapEventToState(
     WeatherEvent event,
   ) async* {
-    // TODO: implement mapEventToState
+    if (event is WeatherRequested) {
+      yield* _mapWeatherRequestedToState(event);
+    } else if (event is WeatherRefreshRequested) {
+      yield* _mapWeatherRefreshRequestedToState(event);
+    }
+  }
+
+  Stream<WeatherState> _mapWeatherRequestedToState(
+    WeatherRequested event,
+  ) async* {
+    yield WeatherLoadInProgress();
+    try {
+      final LocationWeather weather =
+          await weatherRepository.fetchCityWeather(event.city);
+      yield WeatherLoadSuccess(weather: weather);
+    } catch (error) {
+      print(error);
+      yield WeatherLoadFailure();
+    }
+  }
+
+  Stream<WeatherState> _mapWeatherRefreshRequestedToState(
+    WeatherRefreshRequested event,
+  ) async* {
+    try {
+      final LocationWeather weather =
+          await weatherRepository.fetchCityWeather(event.city);
+      yield WeatherLoadSuccess(weather: weather);
+    } catch (_) {}
   }
 }
