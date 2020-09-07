aa8ccbb8c70c53ec2f9e55c36779aa82e48b020f
Ezra Gunn <ezracodes@gmail.com>
2020-09-07 10:12:37 +0800

    Added current position to refresh indicator

--- lib/home/view/home_page.dart
+++ lib/home/view/home_page.dart
@@ -2,10 +2,12 @@ import 'package:fluther/home/blocs/weather/weather_bloc.dart';
 import 'package:fluther/home/models/location_weather/location_weather.dart';
 import 'package:fluther/home/view/widgets/hourly_weather_list.dart';
 import 'package:fluther/home/view/widgets/search_button.dart';
+import 'package:fluther/location/bloc/location/location_bloc.dart';
 import 'package:fluther/main/flavour_config.dart';
 import 'package:fluther/resources/strings.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:intl/intl.dart';
 
 class HomePage extends StatefulWidget {
@@ -21,10 +23,16 @@ class _HomePageState extends State<HomePage> {
 
   @override
   void didChangeDependencies() {
-    BlocProvider.of<WeatherBloc>(context)
-        .add(const WeatherRequested(latitude: 3.1390, longitude: 101.6869));
     setState(() {
       now = DateTime.now();
+    });
+    BlocProvider.of<LocationBloc>(context).listen((LocationState state) {
+      if (state is LocationRequestSuccess) {
+        BlocProvider.of<WeatherBloc>(context).add(WeatherRequested(
+          latitude: state.position.latitude,
+          longitude: state.position.longitude,
+        ));
+      }
     });
     super.didChangeDependencies();
   }
@@ -47,77 +55,87 @@ class _HomePageState extends State<HomePage> {
           if (state is WeatherLoadSuccess) {
             final LocationWeather locationWeather = state.locationWeather;
 
-            return RefreshIndicator(
-              onRefresh: () async {
-                BlocProvider.of<WeatherBloc>(context).add(
-                  const WeatherRefreshRequested(
-                    latitude: 33.441792,
-                    longitude: -94.037689,
-                  ),
-                );
-              },
-              child: ListView(
-                children: <Widget>[
-                  ListTile(
-                    leading: Icon(
-                      Icons.location_on,
-                      color: Theme.of(context).accentColor,
-                    ),
-                    title: Text(
-                      locationWeather.timezone,
-                      style: const TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    subtitle: Text(
-                        '${dayFormat.format(now)} — ${dateFormat.format(now)}'),
-                  ),
-                  const SizedBox(
-                    height: 16.0,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Text(
-                      '${numberFormat.format(locationWeather.current.temp)}°',
-                      textAlign: TextAlign.center,
-                      textScaleFactor: 7.0,
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Theme.of(context).primaryColor,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 16.0,
-                  ),
-                  ListTile(
-                    title: Text(
-                      'Feels like ${numberFormat.format(locationWeather.current.feelsLike)}°',
-                      textScaleFactor: 1.1,
-                      style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    subtitle: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            return BlocBuilder<LocationBloc, LocationState>(
+              builder: (BuildContext context, LocationState state) {
+                if (state is LocationRequestSuccess) {
+                  final Position position = state.position;
+
+                  return RefreshIndicator(
+                    onRefresh: () async {
+                      BlocProvider.of<WeatherBloc>(context).add(
+                        WeatherRefreshRequested(
+                          latitude: position.latitude,
+                          longitude: position.longitude,
+                        ),
+                      );
+                    },
+                    child: ListView(
                       children: <Widget>[
-                        Text(
-                            'Wind: ${locationWeather.current.windSpeed} m/sec'),
-                        Text('Humidity: ${locationWeather.current.humidity}%'),
+                        ListTile(
+                          leading: Icon(
+                            Icons.location_on,
+                            color: Theme.of(context).accentColor,
+                          ),
+                          title: Text(
+                            locationWeather.timezone,
+                            style: const TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          subtitle: Text(
+                              '${dayFormat.format(now)} — ${dateFormat.format(now)}'),
+                        ),
+                        const SizedBox(
+                          height: 16.0,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Text(
+                            '${numberFormat.format(locationWeather.current.temp)}°',
+                            textAlign: TextAlign.center,
+                            textScaleFactor: 7.0,
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Theme.of(context).primaryColor,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 16.0,
+                        ),
+                        ListTile(
+                          title: Text(
+                            'Feels like ${numberFormat.format(locationWeather.current.feelsLike)}°',
+                            textScaleFactor: 1.1,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          subtitle: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Text(
+                                  'Wind: ${locationWeather.current.windSpeed} m/sec'),
+                              Text(
+                                  'Humidity: ${locationWeather.current.humidity}%'),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 16.0,
+                        ),
+                        const ListTile(
+                          title: Text('Hourly'),
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 16.0),
+                          child: Divider(),
+                        ),
+                        HourlyWeatherList(),
                       ],
                     ),
-                  ),
-                  const SizedBox(
-                    height: 16.0,
-                  ),
-                  const ListTile(
-                    title: Text('Hourly'),
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 16.0),
-                    child: Divider(),
-                  ),
-                  HourlyWeatherList(),
-                ],
-              ),
+                  );
+                }
+                return const SizedBox.shrink();
+              },
             );
           } else {
             return const SizedBox.shrink();
